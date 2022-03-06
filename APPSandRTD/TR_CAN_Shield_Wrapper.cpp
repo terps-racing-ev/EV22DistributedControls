1f749587018606e661cf44d99705f898e872f7bc
Yasadu De Silva <49260501+lg4492@users.noreply.github.com>
2022-03-06 13:39:38 -0500

    sendTorque function made
    Code was made last meeting (3/2) but git wasn't cooperating on my computer until now so I couldn't push code to the repo.

--- APPSandRTD/TR_CAN_Shield_Wrapper.cpp
+++ APPSandRTD/TR_CAN_Shield_Wrapper.cpp
@@ -5,13 +5,23 @@ TR_CAN_Shield_Wrapper::TR_CAN_Shield_Wrapper(byte can_id, bool debug) {
 }
 
 void TR_CAN_Shield_Wrapper::sendTorque(int torque) {
+  byte torqueFieldOne = torque;
+  byte torqueFieldTwo = 0;
+
+  if (torque > 255) {
+    torqueFieldOne = 255;
+    torqueFieldTwo = (byte) (torque - 255);
+  }
   
+  byte arrayToSend[8] = {torqueFieldOne, torqueFieldTwo, 0, 0, 1, 0, 0, 0};
+  
+  shield->can_send(0, arrayToSend);
 }
 
 void TR_CAN_Shield_Wrapper::sendRangeFault() {
-  
+  sendTorque(0);
 }
 
 void TR_CAN_Shield_Wrapper::sendTimeoutFault() {
-  
+  sendTorque(0);  
 }
