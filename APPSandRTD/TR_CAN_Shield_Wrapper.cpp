a34896772d7b10c597b3d55e43be15d1682e9758
Yasadu De Silva <49260501+lg4492@users.noreply.github.com>
2022-03-13 15:49:16 -0400

    Created functions to get and update wheel speeds over CAN

--- APPSandRTD/TR_CAN_Shield_Wrapper.cpp
+++ APPSandRTD/TR_CAN_Shield_Wrapper.cpp
@@ -25,3 +25,43 @@ void TR_CAN_Shield_Wrapper::sendRangeFault() {
 void TR_CAN_Shield_Wrapper::sendTimeoutFault() {
   sendTorque(0);  
 }
+
+void TR_CAN_Shield_Wrapper::updateStoredWheelSpeeds() {
+  boolean wheelSpeed1Updated = false;
+  boolean wheelSpeed2Updated = false;
+  boolean wheelSpeed3Updated = false;
+
+  byte receiveBuffer[8];
+  while (!wheelSpeed1Updated && !wheelSpeed2Updated && !wheelSpeed3Updated) {
+    shield->can_receive(0, receiveBuffer);
+    
+    //This is not how you get the CAN ID, need to update that
+    int sendingID = receiveBuffer[0];
+    
+    int data = receiveBuffer[1] + receiveBuffer[2];
+    
+    if (sendingID == wheelSpeedCANID1) {
+      wheelSpeed1 = data;
+      wheelSpeed1Updated = true;
+    } else if (sendingID == wheelSpeedCANID2) {
+      wheelSpeed2 = data;
+      wheelSpeed2Updated = true;
+    } else if (sendingID == wheelSpeedCANID3) {
+      wheelSpeed3 = data;
+      wheelSpeed3Updated = true;
+    }
+  }
+  
+}
+
+int TR_CAN_Shield_Wrapper::getWheelSpeed(int n){
+  if (n == 1) {
+    return wheelSpeed1;
+  } else if (n == 2) {
+    return wheelSpeed2;
+  } else if (n == 3) {
+    return wheelSpeed3;
+  }
+
+  return -1;
+}
--- APPSandRTD/TR_CAN_Shield_Wrapper.h
+++ APPSandRTD/TR_CAN_Shield_Wrapper.h
@@ -11,11 +11,21 @@
 class TR_CAN_Shield_Wrapper {
   private:
     TR_CAN_Shield* shield;
+    /* specific numbers are placeholders */
+    const int wheelSpeedCANID1 = 1;
+    const int wheelSpeedCANID2 = 2;
+    const int wheelSpeedCANID3 = 3;
+
+    int wheelSpeed1;
+    int wheelSpeed2;
+    int wheelSpeed3;
   public:
     TR_CAN_Shield_Wrapper(byte can_id, bool debug);
     void sendTorque(int torque);
     void sendRangeFault();
     void sendTimeoutFault();
+    void updateStoredWheelSpeeds();
+    int getWheelSpeed(int n);
 };
 
 #endif
