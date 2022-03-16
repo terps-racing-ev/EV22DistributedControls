e4f08d578d86f965033e9dc47a63f88af0e35ff9
Yasadu De Silva <49260501+lg4492@users.noreply.github.com>
2022-03-16 19:30:30 -0400

    Update VehicleSpeedCalcControls.cpp

--- APPSandRTD/VehicleSpeedCalcControls.cpp
+++ APPSandRTD/VehicleSpeedCalcControls.cpp
@@ -9,7 +9,6 @@ void VehicleSpeedCalcControls::doAction() {
   wheelSpeed1 = shield->getWheelSpeed(1);
   wheelSpeed2 = shield->getWheelSpeed(2);
   wheelSpeed3 = shield->getWheelSpeed(3);
-
   
   shield->sendAllData();
 }
